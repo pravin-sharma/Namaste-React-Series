79597e49d7f944f0a49f1756334b8abaef685e7f
PRAVIN SHARMA <pravinsharma9610@gmail.com>
2023-07-03 16:47:44 +0530

    Using Functional component

--- src/components/About.js
+++ src/components/About.js
@@ -1,31 +1,19 @@
 import React from 'react';
 import User from './User';
 
-class About extends React.Component {
-  constructor(props) {
-    super(props);
-    console.log('Parent Constructor');
-  }
-
-  componentDidMount() {
-    console.log('Parent componentDidMount');
-  }
-
-  render() {
-    console.log('Parent Render');
-    return (
-      <div className="">
-        <h2>Our Team</h2>
-        <div className="about">
-          <User
-            name="Kaushal Sharma"
-            role="Senior Software Engineer"
-            hobby="Swimming"
-          />
-        </div>
+const About = () => {
+  return (
+    <>
+      <h2>Our Team</h2>
+      <div className="user-card-container">
+        <User
+          name="Kaushal Sharma"
+          role="Senior Software Engineer"
+          hobby="Swimming"
+        />
       </div>
-    );
-  }
-}
+    </>
+  );
+};
 
 export default About;
