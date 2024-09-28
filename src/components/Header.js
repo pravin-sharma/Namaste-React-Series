88c2bb8c20d234c5a794c93eab3c7f7e69cbeabc
PRAVIN SHARMA <pravinsharma9610@gmail.com>
2024-09-28 10:38:33 +0100

    CSS updated - header

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,27 +6,27 @@ export default Header = () => {
   const onlineStatus = useOnlineStatus();
 
   return (
-    <div className="flex flex-wrap justify-between items-center px-5 py-0 m-0 border border-gray-400 rounded-lg">
+    <div className="flex flex-wrap justify-between items-center px-5 py-4 m-3 border border-gray-400 rounded-lg">
       <div className="flex items-center">
-        <img className="w-12" src={LOGO} alt="app logo" />
-        <h1 className="text-orange-500">Mealify</h1>
+        <img className="w-20" src={LOGO} alt="app logo" />
+        <h1 className="text-orange-500 text-3xl">Mealify</h1>
       </div>
-      <div className="flex list-none">
-        <ul>
-          <li className="m-2.5 p-2.5 text-lg">
+      <div>
+        <ul className="flex list-none">
+          <li className="m-2.5 p-2.5 text-xl">
             <Link className="text-black no-underline" to={'/'}>Home</Link>
           </li>
-          <li className="m-2.5 p-2.5 text-lg">
+          <li className="m-2.5 p-2.5 text-xl w">
             <Link className="text-black no-underline" to={'/about'}>About</Link>
           </li>
-          <li className="m-2.5 p-2.5 text-lg">
+          <li className="m-2.5 p-2.5 text-xl">
             <img
               className="w-6"
               src="https://cdn-icons-png.flaticon.com/512/3916/3916598.png"
               alt="cart icon"
             />
           </li>
-          <li className="m-2.5 p-2.5 text-lg">( Status {onlineStatus ? '🟢' : '🔴'} )</li>
+          <li className="m-2.5 p-2.5 text-xl">( Status {onlineStatus ? '🟢' : '🔴'} )</li>
         </ul>
       </div>
     </div>
